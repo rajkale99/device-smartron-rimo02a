c36840c53c56487cddedbaea66c5a8981e1a116a
k2wl <kar2wlan@gmail.com>
2017-01-15 23:26:25 +0530

    rimo02a: needs text relocations
    Change-Id: I2113985ad357a068d6ec4ac68dad2df985f075d7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,12 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+<<<<<<< HEAD
+=======
+TARGET_USES_64_BIT_BINDER := true
+TARGET_NEEDS_TEXT_RELOCATIONS := true
+
+>>>>>>> 389d74e... s2: needs text relocations
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := MSM8952
